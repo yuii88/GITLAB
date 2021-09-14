b3125b1f81cc433d50bba61893fdec619ab4b2a0
spoon261200 <nitid.poon@gmail.com>
2021-09-14 22:31:55 +0700

    Change answer 10->100 and your -> Your

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char Name[40], G[5], answer[100];
   int i;
   do
   {
@@ -9,7 +9,7 @@ void main()
     scanf("%s", Name);
     do
     {
-      printf("what is your blood group [A, B, AB, or O]? ");
+      printf("what is Your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
