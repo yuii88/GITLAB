a71bdd81c0744044065933a5a00abcc3b79bd81a
yuii88 <pimlapas.soraka@mail.kmutt.ac.th>
2021-09-14 22:07:31 +0700

    Change Name -> name and What -> what

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your name? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("what is your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
